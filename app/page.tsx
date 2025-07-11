0396ff97777f1779f5f931c16d4abfc7851a1c91
Felipe Muner <felipe.muner@gmail.com>
2025-07-11 18:36:10 +0700

    Refactor Home component to use MainLayout instead of static content

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
-import Image from "next/image";
+import { MainLayout } from '@/components/layout/main-layout'
 
 export default function Home() {
-  return (
-    <div>lucid notes</div>
-  );
+  return <MainLayout />
 }
